38a9997da22897bbc239561ae15b34eeb3a30850
Karimunf <karimfarhouti@gmail.com>
2021-12-26 11:17:04 +0100

    Prod final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Categorie } from './models/Categorie';
+import { Client } from './models/Client';
+import { CategorieService } from './services/categorie.service';
+import { ClientService } from './services/client.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'caddy-project';
+export class AppComponent implements OnInit {
+  categories: Array<Categorie>;
+  client:Client=new Client();
+  constructor(private categorieService: CategorieService,
+    private clientService:ClientService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.loadClient();
+  }
+
+  getAllCategories() {
+    this.categorieService.getAllCategories()
+      .subscribe(data => {
+        this.categories = data;
+      })
+  }
+
+  loadClient(){
+    this.clientService.loadClient('karimfarhouti@gmail.com').subscribe(
+      data=>{
+        this.client=data;
+        sessionStorage.setItem('client',JSON.stringify(this.client));
+      }
+    )
+    this.getAllCategories();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ProduitComponent } from './components/produit/produit.component';
+import { ListProduitComponent } from './components/list-produit/list-produit.component';
+import { ListCommandeComponent } from './components/list-commande/list-commande.component';
+import { CategorieComponent } from './components/categorie/categorie.component';
+import { CaddyComponent } from './components/caddy/caddy.component';
+import { CommandeComponent } from './components/commande/commande.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProduitComponent,
+    ListProduitComponent,
+    ListCommandeComponent,
+    CategorieComponent,
+    CaddyComponent,
+    CommandeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
